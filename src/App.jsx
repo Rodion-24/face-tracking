c87846093abc03b164cded9540b30a07cb65524f
Rodion-24 <your-email@example.com>
2025-11-27 23:39:47 +0100

    feature/connect video with human detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import Human from '@vladmandic/human';
+
+const human = new Human({
+  cacheSensitivity: 0,
+  debug: false,
+  modelBasePath: '/models/',
+  filter: { enabled: true },
+  face: {
+    enabled: true,
+    detector: { rotation: false },
+    mesh: { enabled: true },
+    iris: { enabled: true },
+    description: true,
+    emotion: true,
+    antispoof: true,
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    async function init() {
+      await human.load();
+      await human.warmup();
+
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+          videoRef.current.srcObject = stream;
+          videoRef.current.onloadedmetadata = () => {
+            videoRef.current.play();
+            detectLoop();
+          };
+        });
+    }
+
+    async function detectLoop() {
+      const result = await human.detect(videoRef.current);
+
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+
+      human.draw.canvas(videoRef.current, canvas);
+      human.draw.all(canvas, result);
+
+      requestAnimationFrame(detectLoop);
+    }
+
+    init();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ position: "relative" }}>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        style={{
+          position: "absolute",
+          width: 600,
+          height: 600,
+          zIndex: 1,
+        }}
+      ></video>
+
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          width: 600,
+          height: 600,
+          zIndex: 2,
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
